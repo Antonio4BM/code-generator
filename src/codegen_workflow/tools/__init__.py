f6734988cfbc343b4358a556508c32a4c43dc7f9
Antonio Briseño <red2013@hotmail.com>
2026-07-14 19:24:40 -0600

    refactor: load read only schemas

--- src/codegen_workflow/tools/__init__.py
+++ src/codegen_workflow/tools/__init__.py
@@ -1,5 +1,10 @@
-"""Coder tools package exports."""
+"""Workspace and reviewer tool package exports."""
 
+from codegen_workflow.tools.readonly import (
+    FORBIDDEN_MUTATION_TOOL_NAMES,
+    READ_ONLY_TOOL_NAMES,
+    ReadOnlyWorkspaceTools,
+)
 from codegen_workflow.tools.workspace import (
     DEFAULT_MAX_FILE_SIZE,
     DEFAULT_MAX_GENERATED_FILES,
@@ -15,6 +20,9 @@ __all__ = [
     "DEFAULT_MAX_GENERATED_FILES",
     "DEFAULT_MAX_PROJECT_SIZE",
     "FORBIDDEN_FILENAMES",
+    "FORBIDDEN_MUTATION_TOOL_NAMES",
+    "READ_ONLY_TOOL_NAMES",
+    "ReadOnlyWorkspaceTools",
     "WorkspaceFileTools",
     "WorkspaceLimitError",
     "WorkspaceSecurityError",
